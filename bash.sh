7e244f9ec283f9102351a4dd84def8a52899f113
RITIK MAKHIJA <35072415+ritik-prog@users.noreply.github.com>
2023-02-26 19:14:30 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,12 @@
+sudo yum update -y
+sudo yum install wget
+sudo amazon-linux-extras install java-openjdk11
+sudo amazon-linux-extras install epel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins -y
+sudo service jenkins start
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
